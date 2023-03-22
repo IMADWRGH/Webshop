fa1bf72c01a177ba7ae5baba5bf95fea90587427
Imad Asserne <imad.asserne@gmail.com>
2023-03-22 23:18:55 +0000

    add component header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,10 +19,12 @@ import  {  MatBadgeModule} from '@angular/material/badge';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
