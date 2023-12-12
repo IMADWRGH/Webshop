6e1f842c80f467504fd4f5a3a4011a2cac1d772e
Imad Asserne <imad.asserne@gmail.com>
2023-12-12 22:18:59 +0100

    format code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [{
-path: ' ',redirectTo:'home',pathMatch:'full',
-},
-{
-  path : 'home',
-  component : HomeComponent,
-}
+const routes: Routes = [
+  {
+    path: ' ', redirectTo: 'home', pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  }
 ];
 
 @NgModule({
