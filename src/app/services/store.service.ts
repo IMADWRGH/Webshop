b67415a1642864cfb851b28f6905940c7eed5b28
Imad Asserne <imad.asserne@gmail.com>
2023-12-29 22:00:04 +0100

    change function

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -24,10 +24,7 @@ export class StoreService {
   getAllCategories(): Observable<Array<string>> {
     return this.http.get<Array<Product>>(`${STORE_BASE_URL}/products`).pipe(
       map(products => {
-        // Assuming each product has a 'category' field
-        const categories = products.map(product => product.category);
-        // If categories are duplicated and you want unique categories, you can use the following line instead:
-        // const categories = [...new Set(products.map(product => product.category))];
+        const categories = [...new Set(products.map(product => product.category))];
         return categories;
       })
     );
